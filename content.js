024df039b472ba9526117491c951caaa71af74fd
nabeki1224 <watanabe.kyo19991224@gmail.com>
2025-03-14 11:52:30 +0900

    feat: color thema

---
+++ content.js
@@ -0,0 +1,4 @@
+// // ここで色を変更します
+// document.body.style.backgroundColor = '#ffffe0'; // 背景色を変更
+// document.querySelector('header').style.backgroundColor = 'fffacd'; // ヘッダーの背景色を変更
+// document.querySelector('header').style.color = '##696969'; // ヘッダーの文字色を白に変更
